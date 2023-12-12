db879f2a1589b8f7c715dc5d1752dcdf28b3cc76
ebachUTSA <eric.bachura@utsa.edu>
2023-12-12 10:50:55 -0600

    Push2 Webday

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from src.utils import insertSQLPandas, selectSQLPandas, getPageSource, getSoup
 
+baseUrl = 'https://www.shodan.io'
 url = 'https://www.shodan.io/search?query=port+3389'
 
 results = getPageSource(url)
 
 soup = getSoup(results) #assuming it's html and not some other markup language
 
-for a in soup.find_all('a'):
-    print(a.get('href'))
+for div in soup.find_all('div',{'class':'result'}):
+    a = div.find('a',{'class':['title']})
+    ip = a.text
+    banner = div.find('pre').text
+    print(ip,banner)
+    print('\n\n\n')
