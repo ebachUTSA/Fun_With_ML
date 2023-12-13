f0370a4e23fa099e23cbef54adc85753143a38be
ebachUTSA <eric.bachura@utsa.edu>
2023-12-13 11:03:48 -0600

    Updated requirements

--- youtube_download.py
+++ youtube_download.py
@@ -1,9 +1,11 @@
-from src.utils import downloadYoutube
+from src.utils import downloadYoutube, sliceVideo
 from src.config import Config
 config = Config()
 
 downLoadDir = f'{config.base_directory}/output/'
 fName = 'UnderPressure'
 url = 'https://www.youtube.com/watch?v=a01QQZyl-_I'
-#comment
-downloadYoutube(url,f"{downLoadDir}{fName}")
+
+# downloadYoutube(url,f"{downLoadDir}{fName}")
+
+sliceVideo(f"{downLoadDir}",fName+'.mp4',fName+"_slice.mp4",32,40)
